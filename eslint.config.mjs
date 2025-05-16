57152f6ea209f79487747b732a63ce4dcfa80972
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-05-16 21:30:34 +0900

    🔧 chore: add vitest plugin and rules for test files in eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import tsEslintParser from '@typescript-eslint/parser';
+import vitest from '@vitest/eslint-plugin';
 import eslintReactiveValueSuffix from 'eslint-plugin-reactive-value-suffix';
 import globals from 'globals';
 import vueEslintParser from 'vue-eslint-parser';
@@ -47,6 +48,24 @@ export default withNuxt([
     rules: {
       'vue/html-self-closing': 'off',
       'vue/multi-word-component-names': 'off',
+    },
+  },
+  {
+    files: ['**/*.spec.js', '**/*.spec.ts', '**/*.test.js', '**/*.test.ts'],
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      'vitest/consistent-test-it': ['error', { fn: 'test', withinDescribe: 'test' }],
+      'vitest/no-conditional-expect': 'error',
+      'vitest/no-conditional-in-test': 'error',
+      'vitest/no-conditional-tests': 'error',
+      'vitest/no-disabled-tests': 'error',
+      'vitest/no-duplicate-hooks': 'error',
+      'vitest/no-test-return-statement': 'error',
+      'vitest/prefer-mock-promise-shorthand': 'error',
+      'vitest/require-hook': 'error',
+      'vitest/require-to-throw-message': 'error',
+      'vitest/require-top-level-describe': ['error', { maxNumberOfTopLevelDescribes: 2 }],
     },
   },
 ]);
