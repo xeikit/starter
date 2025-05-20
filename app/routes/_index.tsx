b7b3eba95a940b315a74b8b9f76ac656b6a6a57b
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-05-20 23:25:40 +0900

    🔧 chore: refactor routing setup and add fs-routes dependency

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import { Welcome } from '../welcome/welcome';
-import type { Route } from './+types/home';
+import type { Route } from './+types/_index';
 
 // biome-ignore lint/correctness/noEmptyPattern: <explanation>
 export function meta({}: Route.MetaArgs) {
